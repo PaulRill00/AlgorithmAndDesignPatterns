b6b676fa8d362d6ab18310fdaa4d27f79b06245c
Paul Rill <paulrill2000@outlook.com>
2021-09-29 17:30:23 +0200

    QuickSort done

--- src/Algorithms/Sorters/QuickSort.cs
+++ src/Algorithms/Sorters/QuickSort.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 
@@ -9,8 +10,60 @@ namespace AD
 
         public override void Sort(List<int> list)
         {
-            // Should be implemented in lecture 4!
-            throw new System.NotImplementedException();
+            this.InternalSort(list, 0, list.Count - 1);
+        }
+
+        private void InternalSort(List<int> list, int low, int high)
+        {
+            if (high < low) return;
+            if (high - low < 10)
+            {
+                new InsertionSort().Sort(list);
+                return;
+            }
+
+            var mid = (int)Math.Floor((decimal)(low + high)) / 2;
+
+            var values = new List<int>() { list[low], list[mid], list[high] };
+            values.Sort();
+            var pivot = values[1];
+            var pivotIndex = list.FindIndex(x => x == pivot);
+
+            // Put pivot at the end
+            var highest = list[high];
+            list[high] = pivot;
+            list[pivotIndex] = highest;
+            high--;
+
+            var i = low;
+            var j = high;
+
+            while (i < j)
+            {
+                while (list[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (list[j] > pivot)
+                {
+                    j--;
+                }
+
+                if (i < j)
+                {
+                    var mem = list[i];
+                    list[i] = list[j];
+                    list[j] = mem;
+                }
+            }
+
+            var bound = list[i];
+            list[i] = pivot;
+            list[high + 1] = bound;
+
+            InternalSort(list, low, j);
+            InternalSort(list, i + 1, high + 1);
         }
     }
 }
