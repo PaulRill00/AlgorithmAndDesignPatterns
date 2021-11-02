1029403541599f189893ac1ed84f14e843d07dc5
Paul Rill <55083975+PaulRill00@users.noreply.github.com>
2021-11-02 09:41:10 +0100

    ShellSort.

--- src/Algorithms/Sorters/ShellSort.cs
+++ src/Algorithms/Sorters/ShellSort.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 
@@ -7,7 +8,20 @@ namespace AD
     {
         public override void Sort(List<int> list)
         {
-            
+            for (var interval = list.Count / 2; interval > 0; interval /= 2)
+            {
+                for (var i = interval; i < list.Count; i++)
+                {
+                    var temp = list[i];
+                    var j = i;
+                    for (; j >= interval && list[j - interval] > temp; j -= interval)
+                    {
+                        list[j] = list[j - interval];
+                    }
+
+                    list[j] = temp;
+                }
+            }
         }
     }
 }
